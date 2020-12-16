dabdab877704dd3f9a47caede25734b5fc43d23d
omarrendon <omarcr.96@gmail.com>
2020-12-15 23:30:48 -0600

    Se agrego la funcion de mostrar el precio total de todos los productos que se agregaron a la canasta de productos

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -3,35 +3,48 @@ import { products } from "../data";
 
 const initialState = {
   item: [],
+  total: 0,
 };
 
 export const cartReducer = (state = initialState, action) => {
   let productIn = [];
   let productOut = [];
+  let totalAdd = 0;
 
   switch (action.type) {
     case ADD_TO_CART:
-      products.map((product) => {
+      products.forEach((product) => {
         if (product.id === action.payload) {
           product = { ...product, amount: (product.amount += 1) };
+          totalAdd = state.total + product.price;
         }
 
         if (product.amount >= 1) {
           productIn = [product, ...productIn];
+          totalAdd = state.total + product.price;
         }
       });
 
       return {
         ...state,
         item: productIn,
+        total: totalAdd,
       };
 
     case REMOVE_FROM_CART:
       productOut = [...state.item];
 
+      const productDiscount = productOut.find(
+        (item) => item.id === action.payload
+      );
+
+      let newTotal =
+        state.total - productDiscount.price * productDiscount.amount;
+
       return {
         ...state,
-        item: productOut.filter((product) => product.id !== action.payload),
+        item: state.item.filter((product) => product.id !== action.payload),
+        total: newTotal,
       };
 
     default:
--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -4,12 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart } from "../actions/cartActions";
 
 export const Cart = () => {
-  const { item } = useSelector(item => item.cart);
+  const { item } = useSelector((item) => item.cart);
+  const { total } = useSelector((total) => total.cart);
   const dispatch = useDispatch();
 
   const handleDeleteProduct = (id) => {
     dispatch(removeFromCart(id));
   };
+
+  console.log(item);
+  console.log(total);
 
   return (
     <div>
@@ -18,17 +22,21 @@ export const Cart = () => {
       </div>
 
       <div className="">
-        {item.map(product => (
+        {item.map((product) => (
           <div className="" key={product.id}>
-              <div className="">
-                <p>NAME: {product.name}</p>
-                <p>PRICE: ${product.price}</p>
-                <p>AMOUNT: {product.amount}</p>
-                <button onClick={ () => handleDeleteProduct(product.id)}>Delete</button>
-                <hr />
-              </div> 
+            <div className="">
+              <p>NAME: {product.name}</p>
+              <p>PRICE: ${product.price}</p>
+              <p>AMOUNT: {product.amount}</p>
+
+              <button onClick={() => handleDeleteProduct(product.id)}>
+                Delete
+              </button>
+              <hr />
+            </div>
           </div>
         ))}
+        {<h3>Total Price: ${total}</h3>}
       </div>
     </div>
   );
