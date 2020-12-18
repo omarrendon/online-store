3bf403f23d9485638b7ded95fa2365d92ad9a214
omar rendon <omarcr.96@gmail.com>
2020-12-17 23:15:47 -0600

    docs agregado

---
+++ docs/precache-manifest.b4c531ccb44ad60ff885a05d761cc2ca.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cddb3a6429e302c25b2d2c200e0569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5726dab38b4472d20df3",
+    "url": "/static/css/main.3042e8ae.chunk.css"
+  },
+  {
+    "revision": "b17bf5314f7eb0c66060",
+    "url": "/static/js/2.642134a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.642134a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5726dab38b4472d20df3",
+    "url": "/static/js/main.3ab9e3ff.chunk.js"
+  },
+  {
+    "revision": "46b09bcd63b731803f50",
+    "url": "/static/js/runtime-main.91260359.js"
+  },
+  {
+    "revision": "21b605062e1b39c6757d1e668deede25",
+    "url": "/static/media/cap.21b60506.jpg"
+  },
+  {
+    "revision": "1b3fdf8136002c2a8363d4eba7e987f6",
+    "url": "/static/media/glasses.1b3fdf81.jpg"
+  },
+  {
+    "revision": "bab5204dc7fbe8a2b12917881bc9e7fc",
+    "url": "/static/media/hoodie.bab5204d.jpg"
+  },
+  {
+    "revision": "18be861ef9bf8130e37b0011dab95bb4",
+    "url": "/static/media/jacket.18be861e.jpg"
+  },
+  {
+    "revision": "ea1c823fdcce6fbf1b47e044fee1677d",
+    "url": "/static/media/jeans.ea1c823f.png"
+  },
+  {
+    "revision": "3b7a0c2691780f5150da1d36699dc768",
+    "url": "/static/media/shoes.3b7a0c26.jpg"
+  },
+  {
+    "revision": "4582c33a98dea02ed2b8b4e8c51755f0",
+    "url": "/static/media/short.4582c33a.jpg"
+  }
+]);
